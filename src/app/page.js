7a8b235e30359ec701adde9ff5ed769a7012153c
Haseeb Chaudhury <98563266+Haseebzz@users.noreply.github.com>
2024-01-03 14:42:10 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -13,15 +13,15 @@ export default function Home() {
           subHeader={'Our story'}
           mainHeader={'About us'}
         />
-    <div className="text-gray-500 max-w-md mx-auto mt-4 font-bold text-2xl">
+        <div className="text-gray-500 max-w-md mx-auto mt-4 font-bold text-2xl">
   <p className="mb-4">
-    You'll find more than just pizza at the top restaurants in Brooklyn. Try some of the local top dishes for a treat. Don't miss the chance to try the popular Cheese Pizza. It's a classic for a reason.
+    You&apos;ll find more than just pizza at the top restaurants in Brooklyn. Try some of the local top dishes for a treat. Dont miss the chance to try the popular Cheese Pizza. It&apos;s a classic for a reason.
   </p>
   <p className="mb-4">
-    Don't forget to add french fries to your order. See why this delicious item is a local favorite.
+    Don&apos;t forget to add french fries to your order. See why this delicious item is a local favorite.
   </p>
   <p className="mb-4">
-    Pino Pizza has been a Slice partner for years, providing delivery and takeout in the community. Curbside pickup at Pino Pizza makes things easy. Place an order, and it'll be ready for you to pick up at your convenience. Save yourself some money with one of the regular deals at Pino Pizza. It's a great way to make your dollars stretch further.
+    Pino Pizza has been a Slice partner for years, providing delivery and takeout in the community. Curbside pickup at Pino Pizza makes things easy. Place an order, and it&apos;ll be ready for you to pick up at your convenience. Save yourself some money with one of the regular deals at Pino Pizza. It&apos;s a great way to make your dollars stretch further.
   </p>
   <p>
     Pino Pizza accepts credit cards. That makes it easy to get your pizza sooner.
